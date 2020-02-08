6106ee0f439b69ec643b9e6b8c9b5bc818e23cd1
CCurl <curlbud-github@yahoo.com>
2020-02-08 11:21:15 -0500

    Renamed file

--- make.bat
+++ make.bat
@@ -9,7 +9,7 @@ goto unknown
 set output=forth-compiler
 set c-files=forth-compiler.c 
 set c-files=%c-files% forth-vm.c
-set c-files=%c-files% forth-prims.c
+set c-files=%c-files% vm-prims.c
 set c-files=%c-files% logger.c
 set c-files=%c-files% ForthVM-Dis.c
 echo making %output% ...
@@ -25,7 +25,7 @@ goto done
 set output=forth
 set c-files=forth.c
 set c-files=%c-files% forth-vm.c
-set c-files=%c-files% forth-prims.c
+set c-files=%c-files% vm-prims.c
 set c-files=%c-files% logger.c
 echo making %output% ...
 echo gcc -g -o %output% %c-files%
