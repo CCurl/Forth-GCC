d10ecf4d533a7a72e5e47e9768d10b7ae4ccb20c
Chris Curl <ccurl@mmm.com>
2020-10-30 14:20:45 -0400

    start of rework

--- block-0100.fs
+++ block-0100.fs
@@ -4,11 +4,12 @@ last here
 variable th th !
 variable tl tl !
 
+: fl tl @ (last) ! th @ (here) ! ;
 : forget-life tl @ (last) ! th @ (here) ! ;
 
 \ ------------- Game of life -------------
-: rows 50 ;
-: cols 100 ;
+: rows 40 ;
+: cols 60 ;
 
 variable grid
 rows cols * ALLOT
@@ -33,8 +34,22 @@ grid-0-fill
 \ ( addr -- 0|1 )
 : is-alive? C@ 1 AND ; INLINE
 
-: cell-sub              \ ( n1 addr1 -- n2 addr2 )
-    SWAP OVER is-alive? + SWAP 1+ ;
+variable n-alive
+
+5555 0
+over c@ 1 and + >R 1+ R>
+
+: cell-sub ( n addr1 -- n addr2 )
+    DUP is-alive? swap + swap 1+ ; INLINE
+
+: neighbors ( addr -- n )
+    0 >R
+        cols - 1- cell-sub cell-sub cell-sub 
+    3 - cols +    cell-sub    1+    cell-sub
+    3 - cols +    cell-sub cell-sub cell-sub
+    R>
+    ;
+
 
 : do-cell               \ ( addr -- )
     DUP T1 !
@@ -86,7 +101,8 @@ grid-0-fill
         1- DUP
     WHILE 2DROP CR ;
 
-: show-grid rows 2-             \ ( -- )
+: show-grid 
+    rows 2-             \ ( -- )
     0 0 gotoxy
     BEGIN
         rows 2- OVER - 1+ show-row
@@ -103,25 +119,115 @@ grid-0-fill
         1- DUP
     WHILE 2DROP ;
 
-: one-cycle CR do-grid show-grid update-grid ;
+: one-cycle do-grid update-grid show-grid ;
 
 : slow-down 1000 * BEGIN 1- DUP WHILE DROP ;
 : st start-timer swap slow-down elapsed ;
 variable delay 1 delay !
 
-: life BEGIN
-    one-cycle DUP . delay @ slow-down
-    1- DUP WHILE DROP ;
+: life 
+    \ show-grid
+    BEGIN
+        one-cycle
+        \ DUP . delay @ slow-down
+        1- DUP 
+    WHILE DROP ;
 
 : set-cell cell-at 1 SWAP C! ;
 : clr-cell cell-at 0 SWAP C! ;
 : cell? cell-at C@ . ;
 
-: reset-life GETTICK 5000 MOD DUP .
-    grid grid-sz CMOVE ;
+variable R
+variable C
+: >cell ( x r c -- ) cell-at ! ;
+: +R R @ 1+ R ! ;
+: +C C @ 1+ C ! ;
+: !RC ( r c -- ) C ! R ! ;
+: >RC ( n -- ) R @ C @ >cell ;
+: >RC+ ( n -- ) >RC +C ;
 
-: go 50 life ; 
-reset-life go
+: >g ( x0 ... xn n -- )
+    C @ >R
+    >R 
+    BEGIN
+        >RC+
+        R> 1- DUP >R
+    WHILE 
+    R> DROP
+    R> C ! ;
+
+: block ( r c -- )
+    OVER    OVER    set-cell
+    OVER    OVER 1+ set-cell
+    OVER 1+ OVER    set-cell
+    SWAP 1+ SWAP 1+ set-cell
+    ;
+
+: herschel-1 ( r c -- ) 
+    R @ >R C @ >R C ! R !
+    1 0 0  3 >g +R
+    1 1 1  3 >g +R
+    1 0 1  3 >g +R
+    0 0 1  3 >g +R
+    R> C ! R> R !
+    ;
+
+: herschel-2 ( r c -- ) 
+    R @ >R C @ >R C ! R !
+    1 0 0  3 >g +R
+    1 0 1  3 >g +R
+    1 1 1  3 >g +R
+    0 0 1  3 >g +R
+    R> C ! R> R !
+    ;
+
+: simkin-gun 
+    R @ >R C @ >R C ! R !
+    R @      C @      block
+    R @      C @  7 + block
+    R @  3 + C @  4 + block
+    R @      C @ 18 + herschel-1
+    R @  8 + C @ 27 + block
+    R @ 11 + C @ 24 + block
+    R @ 11 + C @ 31 + block
+    R> C ! R> R !
+    ;
+
+: gosper-gun ( r c -- )
+    R @ >R C @ >R C ! R !
+    R @ C @
+    R @ 4 + C @      block
+    R @ 2 + R @ 34 + block
+    R @ 2 + C @  4 + !RC
+    0 0 1 1 0 0 0 0  8 >g +R
+    0 1 0 0 0 1 0 0  8 >g +R
+    1 0 0 0 0 0 1 0  8 >g +R
+    1 0 0 0 1 0 1 1  8 >g +R
+    1 0 0 0 0 0 1 0  8 >g +R
+    0 1 0 0 0 1 0 0  8 >g +R
+    0 0 1 1 0 0 0 0  8 >g +R
+    !RC
+    C @ 25 + C !
+    0 0 0 0 1  5 >g +R
+    0 0 1 0 1  5 >g +R
+    1 1 0 0 0  5 >g +R
+    1 1 0 0 0  5 >g +R
+    1 1 0 0 0  5 >g +R
+    0 0 1 0 1  5 >g +R
+    0 0 0 0 1  5 >g +R
+    R> C ! R> R !
+    ;
+
+: reset-life grid-0-fill
+    15 10 simkin-gun
+    \ 2 2 gosper-gun
+    ;
+
+: g0f grid-0-fill ; : sg show-grid ; : x 1 life ;
+
+: go 250 life ; 
+: rl fl 100 load ;
+reset-life
 
 CR sys-info
 \ forget-life CR sys-info
